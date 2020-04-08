a3776c081b1b64761770a2cae79862d486f20bc9
Zhen Huang <zhuang38@hawk.iit.edu>
2020-04-07 21:41:21 -0500

    commit by Zhen Huang

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Change by Zhen Huang
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
