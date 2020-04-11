ab860275311b6dd925934f8939bf88acd9c16392
AnamKhan <anamkhan2021@u.northwestern.edu>
2020-04-11 16:19:29 -0500

    Added database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiFra3hmMEore7QNZdZSUG0h3q3NUKg4I",
+  authDomain: "meetnewfriends-6e495.firebaseapp.com",
+  databaseURL: "https://meetnewfriends-6e495.firebaseio.com",
+  projectId: "meetnewfriends-6e495",
+  storageBucket: "meetnewfriends-6e495.appspot.com",
+  messagingSenderId: "34155896062",
+  appId: "1:34155896062:web:36d0f8b4d5ca94abc899af",
+  measurementId: "G-WC91Q8GSP0"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
+
 
 function App() {
   return (
