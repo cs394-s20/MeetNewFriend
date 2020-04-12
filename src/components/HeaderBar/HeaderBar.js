ce8b4057171afd47c432addb7d99ffaf68f45f2b
Zhen Huang <zhuang38@hawk.iit.edu>
2020-04-12 15:22:19 -0500

    navbar

--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -1,8 +1,24 @@
-import {Title} from "rbx";
 import React from "react";
+import { Button,Navbar,NavbarDropdown, Container} from "rbx";
+import { Link } from "react-router-dom";
+import "../../styles/HeaderBar.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const HeaderBar = ({ title }) => (
-    <Title>{ title || 'loading...' }</Title>
+<Navbar color='primary' variant="dark">
+<Navbar.Brand text-align='center'>
+    
+    <Container>
+    <Button>
+        Back
+    </Button> 
+    </Container>   
+    {title}
+
+
+</Navbar.Brand>  
+</Navbar>        
 );
 
 export default HeaderBar;
