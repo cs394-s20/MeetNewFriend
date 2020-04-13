e45df7c26f82eb316fd9675200da908cfc7e17d5
Yiwei Zhang <happyzyw@umich.edu>
2020-04-12 22:46:22 -0500

    change to dropdown menu

--- src/components/Preference/Preference.js
+++ src/components/Preference/Preference.js
@@ -1,4 +1,4 @@
-import {Button} from "rbx";
+import {Button, Dropdown} from "rbx";
 import React from "react";
 
 const buttonColor = selected => (
@@ -10,17 +10,35 @@ const cuisine = { A: 'American', F: 'French', B: 'British', G: 'Greek', D: 'Medi
 const Preference = ({ state }) => (
     <div className="filter">
         step one - choose a cuisine:
-    <Button.Group hasAddons>
-        { Object.values(cuisine)
-            .map(value =>
-                <Button key={value}
-                        color={ buttonColor(value === state.cuisine) }
-                        onClick={ () => state.setCuisine(value) } >
-                    { value }
-                </Button>
-            )
-        }
-    </Button.Group>
+
+        <Dropdown>
+            <Dropdown.Trigger> <Button color="info"> Cuisine </Button> </Dropdown.Trigger>
+            <Dropdown.Menu>
+                { Object.values(cuisine)
+                    .map(value =>
+                        <Dropdown.Item key={value}
+                                color={ buttonColor(value === state.cuisine) }
+                                onClick={ () => state.setCuisine(value) } >
+                            { value }
+                        </Dropdown.Item>
+                    )
+                }
+                    {/*<Dropdown.Item> 1 </Dropdown.Item>*/}
+                    {/*<Dropdown.Item> 2 </Dropdown.Item>*/}
+            </Dropdown.Menu>
+        </Dropdown>
+
+        {/*<Button.Group hasAddons>*/}
+        {/*    { Object.values(cuisine)*/}
+        {/*        .map(value =>*/}
+        {/*            <Button key={value}*/}
+        {/*                    color={ buttonColor(value === state.cuisine) }*/}
+        {/*                    onClick={ () => state.setCuisine(value) } >*/}
+        {/*                { value }*/}
+        {/*            </Button>*/}
+        {/*        )*/}
+        {/*    }*/}
+        {/*</Button.Group>*/}
     </div>
 );
 
