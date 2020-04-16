c5284d7b332700ee93caa052167d5ac8bf695f14
Zhen Huang <zhuang38@hawk.iit.edu>
2020-04-16 15:46:08 -0500

    modified the Time format for restaurant cards

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -11,6 +11,41 @@ const ref = storage.child("img").child("buffalo.jpg");
 const Restaurant = ({ event }) => {
     const [image, setImage] = useState("");
     const [joined, setJoined] = useState(false);
+    const meals = ['Breakfast','Lunch','Dinner'];
+
+    const matchMeal = duration => {
+        
+        let meal = null;
+
+        let times =  duration.split("-");
+        let startTime = times[0];
+        let endTime = times[1];
+
+        let startTimeHour = parseInt(startTime.split(":")[0]);
+        let endTimeHour = parseInt(endTime.split(":")[0]);
+        let startTimeMinute = parseInt(startTime.split(":")[1]);
+        let endTimeMinute = parseInt(endTime.split(":")[1]);
+
+        if (startTimeHour<12){
+            meal = meals[0];
+        }
+
+        else if (startTimeHour<=18){
+            meal = meals[1];
+        }
+        else{
+            meal = meals[2];
+        }
+
+        
+        let durationHour = (endTimeHour-startTimeHour).toString();
+        let durationMinute = (endTimeMinute-startTimeMinute).toString();
+        let durationFormat = (durationHour==="0") ? (durationMinute+"min") :
+          ((durationMinute==="0" ? (durationHour+"hr") : durationHour+"hr "+durationMinute+"min" ))
+
+        //const time = duration.parse('-');
+        return [meal,startTime,durationFormat];
+    }
 
 
     useEffect(() => {
@@ -33,6 +68,8 @@ const Restaurant = ({ event }) => {
     const handleJoin = () => {
         setJoined(!joined);
     }
+
+    const timeInfo = matchMeal(event["time"]);
 
     return (
         <li>
@@ -60,24 +97,32 @@ const Restaurant = ({ event }) => {
                     </Field>
                     <Field>
                         <Label> Time: </Label>
-                        {event["time"]}
+                        {timeInfo[0]}
+                        <Label> starts: </Label>
+                        {timeInfo[1]}
+                        <Label> for </Label>
+                        {timeInfo[2]}
                     </Field>
+                    
 
-                    <Button color={(() => {
-                    switch (joined) {
-                        case true:   return "success";
-                        case false:  return "warning";
-                        default:     return "warning";
-                    }
-                    })()} size="small" onClick={()=>handleJoin()}>
-                    {(() => {
-                    switch (joined) {
-                        case true:   return "Leave Event";
-                        case false:  return "Join";
-                        default:     return "Join";
-                    }
-                    })()}
-                </Button>
+                    
+                    
+
+                {/*    <Button color={(() => {*/}
+                {/*    switch (joined) {*/}
+                {/*        case true:   return "success";*/}
+                {/*        case false:  return "warning";*/}
+                {/*        default:     return "warning";*/}
+                {/*    }*/}
+                {/*    })()} size="small" onClick={()=>handleJoin()}>*/}
+                {/*    {(() => {*/}
+                {/*    switch (joined) {*/}
+                {/*        case true:   return "Leave Event";*/}
+                {/*        case false:  return "Join";*/}
+                {/*        default:     return "Join";*/}
+                {/*    }*/}
+                {/*    })()}*/}
+                {/*</Button>*/}
 
                 </Card.Content>
             </Card>
