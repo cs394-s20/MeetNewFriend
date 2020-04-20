faadbfc565ea2a8577dc459829d74164750ccfa4
Zhen Huang <zhuang38@hawk.iit.edu>
2020-04-20 17:05:02 -0500

    adding login

--- src/App.js
+++ src/App.js
@@ -2,19 +2,47 @@ import 'firebase/database';
 // import logo from './logo.svg';
 import './App.css';
 import 'rbx/index.css';
+import 'firebase/auth';
 import firebase from './shared/firebase.js'
-import { Button, Container, Title } from 'rbx';
+import { Button,Navbar, Container, Title } from 'rbx';
 import React, { useState, useEffect } from 'react';
 import RestaurantList from "./components/RestaurantList";
 import HeaderBar from "./components/HeaderBar";
+import Login from './components/Login';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import 'firebase/auth';
 
 const db = firebase.database().ref();
 
 const App = () => {
+  const [user,setUser] = useState(null);
+  const [initialRender, setInitialRender] = useState(true);
   const [schedule, setSchedule] = useState({ title: '', events: [] });
-  const [currPage, setCurrPage] = useState("events");
+  const [currPage, setCurrPage] = useState("login");
+
+
+  const handleLogout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        alert("Successfully signed out.");
+      })
+      .catch(() => {
+        alert("Couldn't log out. Try again.");
+      });
+  };
 
   useEffect(() => {
+
+    //authentication
+    firebase.auth().onAuthStateChanged(user=>{
+      setUser(user);
+      setInitialRender(false);
+      console.log(user);
+    })
+
     const handleData = snap => {
       if (snap.val()) setSchedule(snap.val());
     };
@@ -22,35 +50,18 @@ const App = () => {
     return () => { db.off('value', handleData); };
   }, []);
 
-  const updatePage = (l) => {
-    setCurrPage(l);
-  }
 
-  const handlePage = () => {
-    if (currPage === "events"){
-      return (
-      <Container>
-        <HeaderBar title={ schedule.title } />
-        <RestaurantList events={ schedule.events } />
-      </Container>
-      );
-    }
-    else if (currPage === "home"){
-      return (
-        <Container>
-          <HeaderBar title= { schedule.title } />
-        </Container>
-      );
-    }
-  }
+  return (!initialRender && (
+    <Router>
 
-  const page = handlePage();
-
-  return (
-    <Container>
-      {page}
-    </Container>
-    );
+        <HeaderBar title={ schedule.title } user={user}/> 
+      
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/restaurant" component={()=> <RestaurantList events={ schedule.events }/>} />
+        </Switch>
+      </Router>
+    ));
 };
 
 
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,130 +1,26 @@
-// Sign in form elements
-var form = document.getElementById('login-form');
-var emailInput = document.getElementById('email');
-var passwordInput = document.getElementById('password');
-var loginError = document.getElementById('login-error');
+import React from "react";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from '../../../src/shared/firebase.js'
 
-var auth = firebase.auth();
-
-// Helper function to set the login error message
-function setLoginError(message) {
-    loginError.textContent = message;
-    loginError.classList.add('active');
-}
-
-// When the user logs in, send the email and password to firebase.
-// Firebase will determine whether or not the user logged in correctly.
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    var email = emailInput.value;
-    var password = passwordInput.value;
-
-    // First, check that an email and password have been entered,
-    // and if not, display an error message.
-    if (!email || !password) {
-        setLoginError('Email and password are required');
-    } else {
-        // If the login was successful, the .then callback will be called.
-        // Otherwise, the .catch callback will be called,
-        // with an error object containing the error message.
-        auth.signInWithEmailAndPassword(email, password)
-            .then(function (user) {
-
-            })
-            .catch(function (error) {
-                setLoginError(error.message);
-            });
+const Login = () => {
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: "/restaurant",
+    callbacks: {
+      signInSuccessWithAuthResult: () => {
+        alert("Successfully logged in.");
+        return true;
+      }
     }
-});
+  };
 
-// Sign up form elements
-var signUpForm = document.getElementById('signup-form');
-var displayNameInput = document.getElementById('signup-name');
-var signUpEmailInput = document.getElementById('signup-email');
-var signUpPasswordInput = document.getElementById('signup-password');
-var signUpPasswordConfirmInput = document.getElementById('signup-password-confirm');
-var signupError = document.getElementById('signup-error');
-var displayNameInput = document.getElementById('signup-name');
+  return (
+    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+  );
+};
 
-// Boolean to track whether or not a user has just click the button to sign in.
-// This is so that we don't redirect the user who has justed sign up to the chat page
-// before we complete updating their profile and sending them a verification email.
-// If we redirect immediately after creating the account,
-// the operations to update their profile and send a verification email
-// may not get a chance to complete.
-var isSigningUp = false;
-
-// Helper function to set the sign up error message
-function setSignUpError(message) {
-    signupError.textContent = message;
-    signupError.classList.add('active');
-}
-
-// Helper function to reset teh sign up error message
-function clearSignupError() {
-    signupError.textContent = '';
-    signupError.classList.remove('active');
-}
-
-signUpForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    clearSignupError();
-
-    isSigningUp = true;
-
-    var email = signUpEmailInput.value;
-    var password = signUpPasswordInput.value;
-    var passwordConfirm = signUpPasswordConfirmInput.value;
-    var displayNameValue = displayNameInput.value;
-
-    var user = firebase.auth().currentUser;
-    var photoURL = "https://www.gravatar.com/avatar/" + md5(email);
-
-
-
-    // First, check that an email has been provided.
-    if (!email) {
-        setSignUpError('Email is required.');
-    } else if (password !== passwordConfirm) {
-        // Next, check the passwords match
-        setSignUpError('Passwords do not match.');
-    } else if (!displayNameValue) {
-        setSignUpError('Display name is required.');
-    } else {
-        // First, create the user's account
-        auth.createUserWithEmailAndPassword(email, password)
-            .then(function (user) {
-                user.updateProfile({
-                    displayName: displayNameValue,
-                    photoURL: photoURL
-                })
-                    .then(function () {
-                        return user.sendEmailVerification();
-                    })
-                    .then(function () {
-                        window.location.href = 'index.html';
-                    })
-                    .catch(function (error) {
-                        
-                    });
-
-            })
-            .catch(function (error) {
-                // Display error messages
-                setSignUpError(error.message);
-            });
-    }
-});
-auth.onAuthStateChanged(function (user) {
-    // We only want to redirect the user
-    // in this callback if they have just signed in
-    // or if they are returning to the page
-    // and are already signed.
-    if (user && !isSigningUp) {
-        // User is logged in
-
-        window.location.href = 'index.html';
-    }
-});
+export default Login;
