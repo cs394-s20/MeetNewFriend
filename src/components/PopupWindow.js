ec09a5d043500d103b5ef881c5d7ada64fb51fbc
Yiwei Zhang <happyzyw@umich.edu>
2020-04-22 19:56:45 -0500

    change the background color

--- src/components/PopupWindow.js
+++ src/components/PopupWindow.js
@@ -3,15 +3,10 @@ import CreateEvent from "./CreateEvent";
 import Popup from "reactjs-popup";
 import React from "react";
 
-var contentStyle = {
-    width: "40%",
-    background: "orange",
-};
-
 
 const PopupWindow = () => {
     return (
-        <Popup contentStyle={contentStyle} trigger={<Button> Create Event </Button>} position="bottom center" modal closeOnDocumentClick>
+        <Popup trigger={<Button> Create Event </Button>} position="bottom center" modal closeOnDocumentClick>
             <CreateEvent> </CreateEvent>
         </Popup>
     );
