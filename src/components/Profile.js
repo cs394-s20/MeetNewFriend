b24afdd6beed984d5999276ca2612a84f5b01f8d
Emily Qiao <xiaoliq@uw.edu>
2020-04-29 23:04:40 -0500

    small progress in styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,49 +1,53 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { ButtonGroup, Button, Card, Field, Label } from "rbx";
 import Popup from "reactjs-popup";
 import 'firebase/database';
 import 'firebase/auth';
 import firebase from "../shared/firebase";
 
-const Profile = ({person, people}) => {
+const Profile = ({ person, people }) => {
 
     // stylize CSS of cards
     //click name search it in people
-    return(
+    return (
 
         people["profiles"].filter(name => name["name"] === person).map(filteredPerson => (
 
             <li>
-                <Card>
-                    <Field>
-                        <Label> Major: </Label>
-                        {filteredPerson["major"]}
-                    </Field>
-                </Card>
+                <li className='personal_info'>
+                    {/* <Card>
+                        <Field> */}
+                            <Label> Major: 
+                            {filteredPerson["major"]}</Label>
+                            <br/>
+                        {/* </Field>
+                    </Card> */}
 
-                <Card>
-                    <Field>
-                        <Label> Year: </Label>
-                        {filteredPerson["year"]}
-                    </Field>
-                </Card>
+                    {/* <Card>
+                        <Field> */}
+                            <Label> Year: 
+                            {filteredPerson["year"]}</Label><br/>
+                        {/* </Field>
+                    </Card> */}
 
-                <Card>
-                    <Field>
-                        <Label> About Me: </Label>
-                        {filteredPerson["aboutme"]}
-                    </Field>
-                </Card>
+                    {/* <Card>
+                        <Field> */}
+                            <Label> About Me: 
+                            {filteredPerson["aboutme"]}</Label><br/>
+                        {/* </Field>
+                    </Card> */}
+                </li>
 
                 <Card>
                     <Card.Image>
-                        <img src={filteredPerson.imageURL} style={{ width: 300, height: 200 }} alt="Logo" />
+                        <img className='profile_pic'src={filteredPerson.imageURL} style={{ width: 300, height: 200 }} alt="Logo" />
                     </Card.Image>
                 </Card>
 
-                <br/>
+                <br />
+
             </li>
-            )
+        )
         ));
 };
 
